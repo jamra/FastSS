aa0eeb5c95a031b649d79ddf99184376987a1a18
Jacob Amrany <jacob.amrany@gmail.com>
2015-06-15 00:52:18 +0300

    hash table, permutations, tests

--- fss.go
+++ fss.go
@@ -1,10 +1,152 @@
 package fss
 
 import (
+	"fmt"
+	"hash"
+	"io"
+	"strings"
+	"unicode/utf8"
 
+	"github.com/OneOfOne/xxhash/native"
 )
 
 type Fss struct {
-
+	count int
+	hf    hash.Hash32
+	ht    map[uint32][]int
+	d     int
+	fi    map[int]string
 }
 
+func NewFss(d int) *Fss {
+	f := &Fss{
+		hf:    xxhash.New32(),
+		ht:    make(map[uint32][]int),
+		d:     d,
+		count: 0,
+		fi:    make(map[int]string),
+	}
+	return f
+}
+
+func (f *Fss) Insert(w string) {
+	w = strings.ToLower(w)
+	f.count++
+	id := f.count
+	f.fi[id] = w
+	permutations := getpermutations(w, f.d)
+	for _, p := range permutations {
+		hash := f.gethash(p)
+		f.ht[hash] = append(f.ht[hash], id)
+	}
+}
+
+type QueryResult struct {
+	S    string
+	Rank float32
+}
+
+func (f *Fss) Search(q string) []QueryResult {
+	q = strings.ToLower(q)
+	results := make([]QueryResult, 0)
+
+	permutations := getpermutations(q, f.d)
+
+	for _, p := range permutations {
+		hash := f.gethash(p)
+		vals, ok := f.ht[hash]
+		if ok {
+			for _, val := range vals {
+				str, _ := f.fi[val]
+				fmt.Println(str)
+				qr := QueryResult{
+					S: str,
+				}
+				results = append(results, qr)
+			}
+		}
+	}
+	return results
+}
+
+func (f *Fss) gethash(w string) uint32 {
+	f.hf.Reset()
+	r := strings.NewReader(w)
+	io.Copy(f.hf, r)
+	hash := f.hf.Sum32()
+	return hash
+}
+
+type loc struct {
+	index int
+	width int
+}
+
+//getpermutations takes a word and an edit distance and returns all strings in the deletion neighborhood
+//specified by the edit distance
+func getpermutations(word string, del int) []string {
+	locs := make([]loc, 0)
+
+	for i, w := 0, 0; i < len(word); i += w {
+		_, width := utf8.DecodeRuneInString(word[i:])
+		locs = append(locs, loc{
+			index: i,
+			width: width,
+		})
+		w = width
+	}
+
+	perms := permutations(locs, del)
+	results := stringsFromPermutations(word, locs, perms)
+
+	return results
+}
+
+func permutations(locs []loc, del int) [][]loc {
+	results := make([][]loc, 0)
+
+	for d := 1; d <= del; d++ {
+		results = append(results, permutationsR(locs, len(locs)-d)...)
+	}
+
+	return results
+}
+
+//permutationsR takes in a length and a deletion distance. It then recursively
+//calls itself, assembling every combination of len(loc) choose length
+func permutationsR(locs []loc, length int) [][]loc {
+	results := make([][]loc, 0)
+
+	for i, cur := range locs {
+		if length == 1 {
+			results = append(results, []loc{cur})
+		} else {
+			items := permutationsR(locs[i+1:], length-1)
+			for _, next := range items {
+				tmp := make([]loc, 0)
+				tmp = append(tmp, cur)
+				tmp = append(tmp, next...)
+				results = append(results, tmp)
+			}
+		}
+	}
+
+	return results
+}
+
+//stringsFromPermutations takes an original string, a list of its rune start indices and widths, and
+//an array of inclusions (perm). It goes through each deletion item and builds a string from the original.
+//The return value is an array of strings that make up each version of the original string with associated deletions.
+func stringsFromPermutations(w string, locs []loc, perms [][]loc) []string {
+	results := make([]string, 0)
+	results = append(results, w)
+
+	for _, v := range perms {
+		s := ""
+		for _, l := range v {
+			s += w[l.index : l.index+l.width]
+		}
+		results = append(results, s)
+	}
+	return results
+}
--- fss_test.go
+++ fss_test.go
@@ -1,21 +1,100 @@
 package fss
 
-import "testing"
+import (
+	"bufio"
+	"os"
+	"testing"
+)
 
-func TestBuildFFS(t *testing.T) {
-	//Builds the data structure. This may need to be benchmarked and memory measured.
+var fss *Fss
+
+func TestBuildFSS(t *testing.T) {
+	fss = NewFss(2)
+	filename := "/usr/share/dict/words"
+	f, err := os.Open(filename)
+	if err != nil {
+		t.Error("Unable to open file %s", filename)
+	}
+
+	scanner := bufio.NewScanner(f)
+	count := 0
+	for scanner.Scan() {
+		count++
+		if count == 1000 {
+			break
+		}
+		fss.Insert(scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		t.Errorf("Could not read lines from file `%s`", filename)
+	}
 }
 
+//I'm not sure if this is needed as I am using a third party library that is supposed to be tested by industry standards
 func TestHashWord(t *testing.T) {
-	//Hash should work similar to Boost v1.36.0
+
 }
 
 func TestSplitWord(t *testing.T) {
-	//Large words get split
+	//TODO: Add more words with different distances.
+	table := []struct {
+		word     string
+		distance int
+		expected []string
+	}{
+		{word: "table", distance: 1, expected: []string{"tabl", "tabe", "tale", "tble", "able"}},
+	}
+
+	for _, tab := range table {
+		perm := getpermutations(tab.word, tab.distance+1)
+
+		for _, str := range tab.expected {
+			if !contains(perm, str) {
+				t.Errorf("Expected string %s.", str)
+			}
+		}
+	}
+}
+
+func qcontains(strs []QueryResult, s string) bool {
+	for _, str := range strs {
+		if str.S == s {
+			return true
+		}
+	}
+	return false
+}
+
+func contains(strs []string, s string) bool {
+	for _, str := range strs {
+		if str == s {
+			return true
+		}
+	}
+	return false
 }
 
 func TestLookup(t *testing.T) {
-	//Simply test if looking up a word returns all words within edit distance d
+	//TODO: This should build an FSS for each query item. This would make sure I can search
+	//different sizes of deletion neighborhoods
+	table := []struct {
+		query    string
+		matches  []string
+		distance int
+	}{
+		{query: "abaft", matches: []string{"abaft"}, distance: 0},
+		{query: "abaft", matches: []string{"abaft", "abaff"}, distance: 1},
+	}
+
+	for _, tabl := range table {
+		results := fss.Search(tabl.query)
+		for _, w := range tabl.matches {
+			if !qcontains(results, w) {
+				t.Errorf("Expected %s", w)
+			}
+		}
+	}
 }
 
 func TestSplitQuery(t *testing.T) {
